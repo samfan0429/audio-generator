b2db11a915f134be97ab49b2795e6d9ebe56cd1f
Sun Gyu Park <spark2@macalester.edu>
2021-06-15 18:29:33 +0900

    refresh the file reading issues

--- part6-sampling-portaudio/src/simple.cpp
+++ part6-sampling-portaudio/src/simple.cpp
@@ -1,19 +1,25 @@
-#include "pa.h"
-#include "math.h"
+# include "pa.h"
+# include <math.h>
+# include <iostream>
+
 #define TWO_PI 6.2831853
 
-double phase = 0, step = TWO_PI*440/44100.0;
+double phase = 0, step = TWO_PI*440.0/44100.0;
 
 void paFunc(const float* in, float* out, long frames, void* data){    
 
     for(int i = 0; i < frames; i++ ){
             phase += step;
-            *out++ = sin(phase)*0.5;
+            *out++ = 0.7*(sin(phase)+0.874*sin(3*phase)+0.684*sin(4*phase));
+            // std::cout << *out << std::endl;
         }
 }
 
 int main() {
+    // std::cout << "Working? " << std::endl;
+    
     Pa a(paFunc, NULL);
     a.start(Pa::waitForKey);
+    
     return 0;
 }
