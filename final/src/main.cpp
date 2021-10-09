7ca181832abcfc954020432ff87a19d70426ed36
Sun Gyu Park <spark2@macalester.edu>
2021-10-10 00:13:43 +0900

    Amplitude change.

--- final/src/main.cpp
+++ final/src/main.cpp
@@ -24,14 +24,14 @@ void paFunc(const float* in, float* out, long frames, void* data){
     SM* tmp = reinterpret_cast<SM*>(data);
     for(int i = 0; i < frames; i++ ){
             phase += step;
-            *out++ = sin(tmp->freqs[0]*phase)+\
-            tmp->amps[0]*sin(tmp->freqs[0]*2*phase)+\
-            tmp->amps[1]*sin(tmp->freqs[0]*3*phase)+\
-            tmp->amps[2]*sin(tmp->freqs[0]*4*phase)+\
-            tmp->amps[3]*sin(tmp->freqs[0]*5*phase)+\
-            tmp->amps[4]*sin(tmp->freqs[0]*6*phase)+\
-            tmp->amps[5]*sin(tmp->freqs[0]*7*phase)+\
-            tmp->amps[6]*sin(tmp->freqs[0]*8*phase);
+            *out++ = 2*sin(tmp->freqs[0]*phase)+\
+            2*tmp->amps[0]*sin(tmp->freqs[0]*2*phase)+\
+            2*tmp->amps[1]*sin(tmp->freqs[0]*3*phase)+\
+            2*tmp->amps[2]*sin(tmp->freqs[0]*4*phase)+\
+            2*tmp->amps[3]*sin(tmp->freqs[0]*5*phase)+\
+            2*tmp->amps[4]*sin(tmp->freqs[0]*6*phase)+\
+            2*tmp->amps[5]*sin(tmp->freqs[0]*7*phase)+\
+            2*tmp->amps[6]*sin(tmp->freqs[0]*8*phase);
         }
 }
 
